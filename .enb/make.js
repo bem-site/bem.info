edc2fa5034e80cdb8b1a0dc3a97b0cc528f16e3c
Vitaly Harisov <vitaly@yandex-team.ru>
2015-09-21 14:59:04 +0300

    cleanup

--- .enb/make.js
+++ .enb/make.js
@@ -30,7 +30,8 @@ var techs = {
     ];
 
 module.exports = function(config) {
-    var isProd = process.env.YENV === 'production';
+    var env = process.env;
+    var isProd = env.YENV === 'production';
 
     config.nodes('*.bundles/*', function(nodeConfig) {
         nodeConfig.addTechs([
@@ -41,49 +42,17 @@ module.exports = function(config) {
             [enbBemTechs.files],
 
             // css
-            [techs.css, {
-                target: '?.css'
-                // , autoprefixer: {
-                //     browsers: ['last 2 versions', 'ie 10', 'opera 12.1']
-                // }
-            }],
+            [techs.css, { target: '?.css' }],
 
             // bemtree
-            [techs.bemtree, { devMode: process.env.BEMTREE_ENV === 'development' }],
+            [techs.bemtree, { devMode: env.BEMTREE_ENV === 'development' }],
 
             // bemhtml
-            [techs.bemhtml, { devMode: process.env.BEMHTML_ENV === 'development' }],
+            [techs.bemhtml, { devMode: env.BEMHTML_ENV === 'development' }],
 
-            // client bemhtml
-/*            
-            [enbBemTechs.depsByTechToBemdecl, {
-                target: '?.bemhtml.bemdecl.js',
-                sourceTech: 'js',
-                destTech: 'bemhtml'
-            }],
-            [enbBemTechs.deps, {
-                target: '?.bemhtml.deps.js',
-                bemdeclFile: '?.bemhtml.bemdecl.js'
-            }],
-            [enbBemTechs.files, {
-                depsFile: '?.bemhtml.deps.js',
-                filesTarget: '?.bemhtml.files',
-                dirsTarget: '?.bemhtml.dirs'
-            }],
-            [techs.bemhtml, {
-                target: '?.browser.bemhtml.js',
-                filesTarget: '?.bemhtml.files',
-                devMode: process.env.BEMHTML_ENV === 'development'
-            }],
-*/
             // js
             [techs.browserJs, { includeYM: true }],
-/*
-            [techs.fileMerge, {
-                target: '?.js',
-                sources: ['?.browser.bemhtml.js', '?.browser.js']
-            }],
-*/
+
             // borschik
             [techs.borschik, { sourceTarget: '?.browser.js', destTarget: '?.min.js', minify: isProd }],
             [techs.borschik, { sourceTarget: '?.css', destTarget: '?.min.css', tech: 'cleancss', minify: isProd }]
