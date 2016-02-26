0f161ca38397902fd63881f9abc6831088b50fe7
Vitaly Harisov <vitaly@yandex-team.ru>
2016-02-26 16:37:54 +0300

    platform footer

---
+++ blocks/promo/promo-footer/_site/promo-footer_site_platform.bemtree.js
@@ -0,0 +1,15 @@
+block('promo-footer').mod('site', 'platform').content()([
+    {
+        elem : 'text',
+        content : 'You should <strong>use BEM</strong> too'
+    },
+    {
+        elem : 'text',
+        content : 'There are no reason not to do so'
+    },
+    {
+        block : 'promo-action',
+        url : 'key-concepts/',
+        content : 'Документация'
+    }
+]);
