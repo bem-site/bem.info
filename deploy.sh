47b0577758d60e5c9a7a23763a30efac0441b7d8
Vitaly Harisov <vitaly@yandex-team.ru>
2016-03-19 10:01:59 +0300

    Change usage to ./deploy.sh <github-username>

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+A=`echo $1 | grep git`
+
+if [[ -z $1 || $A == $1 ]]; then
+    echo "Usage:"
+    echo "    $0 <github-username>"
+    exit 1;
+fi
+
 cd output/bem.info
 
 # init new repo
 git init
-git remote add origin $1
+git remote add origin git@github.com:$1/bem.info.git
 
 git checkout -b gh-pages
 
