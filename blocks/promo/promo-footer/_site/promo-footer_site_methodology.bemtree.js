a4296a4a32f57bc1d780480ad750d1d226d19856
Vitaly Harisov <vitaly@harisov.name>
2016-04-11 20:04:40 +0300

    i18n for promo-footer_site_methodology

--- blocks/promo/promo-footer/_site/promo-footer_site_methodology.bemtree.js
+++ blocks/promo/promo-footer/_site/promo-footer_site_methodology.bemtree.js
@@ -1,29 +1,30 @@
-block('promo-footer').mod('site', 'methodology').content()([
-    {
-        block: 'promo-title',
-        mods: { color: 'white' },
-        content: [
-            'Вы тоже ',
-            // 'You should ',
-            {
-                block: 'promo-highlight',
-                content: 'используйте'
-                // 'use',
-            },
-            ' ',
-            { block: 'bem' }
-            // ' too'
-        ]
-    },
-    {
-        block: 'promo-title',
-        mods: { color: 'white' },
-        content: 'Нет повода не попробовать'
-        // 'There are no reason not to do so'
-    },
-    {
-        block: 'promo-action',
-        attrs: { href: 'key-concepts/' },
-        content: 'Документация'
-    }
-]);
+block('promo-footer').mod('site', 'methodology').content()(function() {
+    return [
+        {
+            block: 'promo-title',
+            mods: { color: 'white' },
+            content: [
+                this.i18n(this.block, 'you'), // 'You should',
+                ' ',
+                {
+                    block: 'promo-highlight',
+                    content: this.i18n(this.block, 'use') // 'use'
+                },
+                ' ',
+                { block: 'bem' },
+                ' ',
+                this.i18n(this.block, 'too') // ' too'
+            ]
+        },
+        {
+            block: 'promo-title',
+            mods: { color: 'white' },
+            content: this.i18n(this.block, 'reason') // 'There are no reason not to do so'
+        },
+        {
+            block: 'promo-action',
+            attrs: { href: 'key-concepts/' },
+            content: this.i18n(this.block, 'doc') // 'Documentation'
+        }
+    ]
+});
---
+++ blocks/promo/promo-footer/_site/promo-footer_site_methodology.i18n/en.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'promo-footer': {
+        you: 'You should',
+        use: 'use',
+        too: 'too',
+        reason: 'There are no reason not to do so',
+        doc: 'Documentaion'
+    }
+};
+
