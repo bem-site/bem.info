f4f5f00a83de244ba4aa559bf657eae4366e13d6
Anna Tyutyunnik <anya.tyutyunnik@yandex.ru>
2016-04-19 22:10:08 +0300

    lego update

--- blocks/common/legos/legos.bemhtml.js
+++ blocks/common/legos/legos.bemhtml.js
@@ -1,18 +1,23 @@
 block('legos')(
 	content()(function() {
-		var blocks = [];
+        /* @random - random value -1 or 1
+         * @w - block width
+         * @h - block height
+         */
+		var blocks = [],
+            h = 0, w;
 
-		for (var i = 0; i < 28; i++) {
-			var w = Math.floor(Math.random() * 3) + 1;
-			var h = Math.floor(Math.random() * 5) + 1;
+        for (var i = 0; i < 28; i++) {
+                w = Math.floor(Math.random() * 3) + 1;
+                h = 1 + ({ '4': -1, '1': 1 }[h] || Math.floor(Math.random() * 3) || -1);
 
-			blocks.push(
-				{
-					elem: 'lego',
-					elemMods: { width: w > 1 ? w : undefined, height: h > 1 ? h : undefined }
-				}
-			);
-		}
+            blocks.push(
+                {
+                    elem: 'lego',
+                    elemMods: { width: w > 1 && w, height: h > 1 && h }
+                }
+            );
+        }
 
 		return blocks;
 	})
