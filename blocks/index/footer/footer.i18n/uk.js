5ab418a551b056e32eb1b88f8ac71d6d3ae4e634
Vladimir Grinenko <i@tadatuta.com>
2016-05-14 15:34:46 +0300

    uk: Add i18n translations

---
+++ blocks/index/footer/footer.i18n/uk.js
@@ -0,0 +1,8 @@
+module.exports = {
+    footer: {
+        domain: 'Ядвіга і Яцек Даниц — спасибі за домен!',
+        made: 'з гордістю зроблений в',
+        yandex: 'Яндексі',
+        'yandex-url': 'https://yandex.ua/company/'
+    }
+};
