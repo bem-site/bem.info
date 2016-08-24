b69ed182aaf9e42ec165ccfc32ce7085f1034785
Alexej Yaroshevich <qfox@ya.ru>
2016-08-24 14:24:48 +0300

    feat: add lang attr for html tag (#131)
    * feat: add lang attr for html tag
    
    * Update page.bemhtml.js
    
    * Update page.bemhtml.js
    
    * Update page.bemhtml.js

---
+++ blocks/common/page/page.bemhtml.js
@@ -0,0 +1,10 @@
+block('page').def()(function() {
+    var lang = this.ctx.lang;
+    return applyNext().map(function(node) {
+        if (node.tag === 'html') {
+            node.attrs || (node.attrs = {});
+            node.attrs.lang = lang || 'en';
+        }
+        return node;
+    });
+});
