50f1238a21cebb0260a49331f08758a588715972
Vitaly Harisov <vitaly@yandex-team.ru>
2017-07-03 04:22:33 +0300

    article-wrap: social-likes moved to article/articles

--- blocks/common/article-wrap/__articles/article-wrap__articles.bemtree.js
+++ blocks/common/article-wrap/__articles/article-wrap__articles.bemtree.js
@@ -1,8 +1,16 @@
 block('article-wrap').mode('articles')(
     match(function() { return this.data.page.type === 'articles' })(function() {
+        var data = this.data,
+            page = data.page;
+
         return [
             { block: 'articles' },
             // { block: 'articles-add' },
+            {
+                block: 'social-likes',
+                params: page.head.meta,
+                lang: data.lang
+            }
         ];
     })
 );
