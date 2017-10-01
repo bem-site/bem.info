790e657ceadfaebd7043046bbdcfe0baeaa09a37
Aleksandr Fursenko <syngularity@ya.ru>
2017-10-01 18:22:45 +0300

    Updating nginx template configs

--- tools/nginx-server-template.js
+++ tools/nginx-server-template.js
@@ -1,4 +1,9 @@
 module.exports = `
+
+upstream bemforum_{LANG}_server {
+    server https://127.0.0.1:8079 fail_timeout=0;
+}
+
 server {
     listen 8080;
 
@@ -13,6 +18,9 @@ server {
 
     location / {
         try_files $uri $uri/ @redirects;
+    }
+    location /forum/ {
+        proxy_pass http://bemforum_{LANG}_server;
     }
     location @redirects {
         {REDIRECTS}
