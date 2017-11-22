c54a7ac7c48fd240b0ef1c8921b9260fc3bd2de1
Anna Tyutyunnik <anya.tyutyunnik@yandex.ru>
2017-11-22 14:05:00 +0300

    New side-menu for desktop (#203)

--- blocks/common/article-wrap/__article/article-wrap__article.bemtree.js
+++ blocks/common/article-wrap/__article/article-wrap__article.bemtree.js
@@ -4,19 +4,16 @@ block('article-wrap').mode('article')(
         return !type || type === 'lib' || type === 'versioned';
     })(function() {
         var data = this.data,
-            page = data.page,
-            isShowRewind = !page.type || page.type === 'versioned';
+            page = data.page;
 
         return [
             { block: 'article' },
-            isShowRewind && { block: 'article-rewind', mods: { type: 'floating' } },
             { block: 'article-amendments' },
             {
                 block: 'social-likes',
                 params: page.head.meta,
                 lang: data.lang
-            },
-            isShowRewind && { block: 'article-rewind', mods: { type: 'static', lang: data.lang } }
+            }
         ];
     })
 );
