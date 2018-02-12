4c0a56365798719b3a257bf7ba654b81083b96e9
Vladimir Grinenko <i@tadatuta.com>
2018-02-12 09:25:12 +0300

    Fix article-amendments

--- blocks/common/article-amendments/article-amendments.bemtree.js
+++ blocks/common/article-amendments/article-amendments.bemtree.js
@@ -1,15 +1,15 @@
 block('article-amendments').content()(function() {
     return [
-        this.i18n(this.block, 'text'), // 'If you notice a mistake or want something to supplement the article, you can always write to us at '
+        this.i18n(this.block, 'text'), // 'Report an error on '
         {
-            elem: 'edit',
+            elem: 'issue',
             mix: { elem: 'link' },
             content: this.i18n(this.block, 'issue'), // Github
             url: this.data.page.source || ''
         },
-        this.i18n(this.block, 'edit'), // , or correct an article using
+        this.i18n(this.block, 'edit'), // , or correct using
         {
-            elem: 'issue',
+            elem: 'edit',
             mix: { elem: 'link' },
             content: 'prose.io.',
             url: this.data.page.source || ''
