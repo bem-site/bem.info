869b482d3604fefe82c3d1e924118202ed180276
tserakhau <tserakhau@yandex-team.ru>
2018-03-21 11:51:36 +0300

    Добавляет недостающий текст на промо странице

--- blocks/platform-index/promo-content/promo-content.i18n/en.js
+++ blocks/platform-index/promo-content/promo-content.i18n/en.js
@@ -3,6 +3,7 @@ module.exports = {
         text0: 'platform',
         text1: 'Quick start',
         text2: 'An ecosystem of web development, providing technology and ready-made components libraries.',
-        text3: 'Allows quickly create, easily scale and support projects of any complexity.'
+        text3: 'Allows quickly create, easily scale and support projects of any complexity.',
+        text4: 'By programming names of directories and files.'
     }
 };
