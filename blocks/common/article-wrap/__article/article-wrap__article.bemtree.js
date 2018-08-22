8598c129f750c64dc38d79dfa6adf65754489fc9
Vitaly Harisov <vitaly@yandex-team.ru>
2018-08-22 16:16:11 +0300

    New design, untested implementation

--- blocks/common/article-wrap/__article/article-wrap__article.bemtree.js
+++
@@ -1,9 +0,0 @@
-block('article-wrap').mode('article')(
-    match(function() {
-        var type = this.data.page.type;
-        return !type || type === 'lib' || type === 'versioned';
-    })(function() {
-        return { block: 'article' };
-    })
-);
-
