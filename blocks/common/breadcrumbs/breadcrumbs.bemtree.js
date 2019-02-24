6785cff60815a339ef25acfc8bfb02de2673bc82
Vitaly Harisov <vitaly@yandex-team.ru>
2019-02-25 02:26:24 +0300

    New design, take two

--- blocks/common/breadcrumbs/breadcrumbs.bemtree.js
+++ blocks/common/breadcrumbs/breadcrumbs.bemtree.js
@@ -26,3 +26,37 @@ block('breadcrumbs').content()(function() {
 
     return result;
 });
+
+block('breadcrumbs').mode('dropdown')(function() {
+    var data = this.data,
+        page = data.page,
+        root = data.root,
+        site = page.site;
+
+    if (data.libs[page.library] && (page.type === 'lib' || page.type === 'versioned')) {
+        var version = page.version;
+
+        return {
+            elem: 'item',
+            mix: { elem: 'version' },
+            content: {
+                block: 'select',
+                mods: {
+                    header: true,
+                    mode: 'radio-check',
+                    theme: 'islands',
+                    size: 'l'
+                },
+                text: '',
+                options: data.libs[page.library].reduce(function(acc, ver) {
+                    (ver !== version) && acc.push({
+                        val: root + site.replace(version + '/', '') + ver + '/',
+                        text: ver
+                    });
+
+                    return acc;
+                }, []).reverse()
+            }
+        };
+    }
+});
---
+++ blocks/tutorials-index/promo-content/promo-content.i18n/en.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'promo-content': {
+        text0: 'tutorials',
+    }
+};
