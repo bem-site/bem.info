bc6c25adc8fb15709f7a8fea92a347e9e4088f21
Vitaly Harisov <vitaly@yandex-team.ru>
2019-02-26 00:14:58 +0300

    redirects/toolbox: bem-naming -> bem-naming-entity

--- content/redirects/toolbox.js
+++ content/redirects/toolbox.js
@@ -1,5 +1,9 @@
 module.exports = [
     {
+        exp: '^/toolbox/sdk/bem-naming/(.*)',
+        now: '/toolbox/sdk/bem-naming-entity/$1',
+    },
+    {
         exp: '^/toolbox/enb/enb-(.*)/(.*)',
         now: '/toolbox/enb/packages/enb-$1/$2',
     },
